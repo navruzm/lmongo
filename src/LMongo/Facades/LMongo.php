ed70dab892133b14b5e511c5f4674ec04f8a90ac
Mustafa Navruz <mustafanavruz@gmail.com>
2013-01-12 21:29:16 +0200

    adding Facade

---
+++ src/LMongo/Facades/LMongo.php
@@ -0,0 +1,17 @@
+<?php namespace LMongo\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class LMongo extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'mongo';
+	}
+
+}
